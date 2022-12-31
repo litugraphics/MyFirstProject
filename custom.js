1f411192d4c91aea6832d2c282efdf2084c3e187
litu rahman <lrahmanlitu@gmail.com>
2022-12-31 13:29:35 +0600

    my first commit

---
+++ custom.js
@@ -0,0 +1,27 @@
+// let newp = document.createElement("p")
+// let text =document.createTextNode("click me,I am here")
+// let box = document.getElementsByClassName("box")
+// newp.appendChild(text)
+// // document.body.appendChild(newp)
+// let btn = document.getElementById("btn")
+
+// btn.addEventListener("mouseover",function(){
+//  document.body.appendChild(newp)
+
+// })
+// btn.addEventListener("mouseout",function(){
+//  document.body.removeChild(newp)
+
+// })
+
+
+
+let arr = ["masum","kobir","sikder"];
+
+let obj = {
+    name: "mamun",
+    age: 45
+}
+
+// console.log(arr)
+console.log(obj.name)
